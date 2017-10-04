a74c58bb4e499f17511640cb1de24d3c9d60b5b3
guyfarkash <guyfarkash@gmail.com>
2017-10-04 18:32:48 -0400

    graph.py update

--- graph.py
+++ graph.py
@@ -1,38 +1,65 @@
 import scipy.io as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as ptch
 import scipy.fftpack as fft
+import os
 
-# fh = open("/home/g/Desktop/Full Duplex 6001/Results/multi_sine/tx_file", "rb")
-# fh = open("/home/g/Desktop/Full Duplex 6001/Results/multi_sine/rx_file", "rb")
-fh = open("/home/g/Desktop/Full Duplex 6001/Results/multi_sine/y_clean_file", "rb")
+file = ['tx_file', 'rx_file', 'y_clean_file']
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-y =np.fromfile(fh, dtype=float)
-# print (y.shape)
 
-x = np.arange(y.shape[0])
+fh = open(dir_path + "/" + file[0], "rb")
+tx = np.fromfile(fh, dtype=float)
+
+fh = open(dir_path + "/" + file[1], "rb")
+rx = np.fromfile(fh, dtype=float)
+
+fh = open(dir_path + "/" + file[2], "rb")
+rx_clean = np.fromfile(fh, dtype=float)
+
+x = np.arange(tx.shape[0])
 print (x.shape)
 
+start = 0
+stop = start + 20440
 # start = 34000
 # stop = 37000
-start = 40000
-stop = 60000
-y1 = y[start:stop]
+# start = 40000
+# stop = 60000
+
+
 x1 = x[start:stop]
+tx1 = tx[start:stop]
+rx1 = rx[start:stop]
+rx_clean1 = rx_clean[start:stop]
 
-# ds = 2
-# y1 = y[0:y.shape[0]:ds]
-# x1 = x[0:y.shape[0]:ds]
 
-y1f = fft.fft(y1)
-x1f = np.arange(y1f.shape[0])
-# x1f = np.linspace(0.0, 1.0/(2.0*T), size/2)
+tx1f = fft.fft(tx1)
+rx1f = fft.fft(rx1)
+rx_clean1f = fft.fft(rx_clean1)
 
-plt.figure(1)
-plt.plot(x1,y1)
+bw = 5 * 10 ** 5
+step = 2 * bw / tx1f.shape[0]
+x1f = np.arange(- bw, bw, step)
 
+# plt.figure(1)
+
+# plt.plot(x1, np.log10(tx1), 'r', x1, np.log10(rx1), 'b', x1, np.log10(rx_clean1), 'g', alpha=0.5)
+
+# red_patch = ptch.Patch(color='r', label='TX')
+# blue_patch = ptch.Patch(color='b', label='RX')
+# green_patch = ptch.Patch(color='g', label='RX Clean')
+# plt.legend(handles=[red_patch, blue_patch, green_patch])
+
+p_scale = 5.5
 plt.figure(2)
-plt.plot(x1f,y1f)
+plt.plot(x1f, p_scale * np.log10(tx1f), 'r', x1f, p_scale * np.log10(rx1f), 'b', x1f, p_scale * np.log10(rx_clean1f), 'g', alpha=0.5)
+
+red_patch = ptch.Patch(color='r', label='TX')
+blue_patch = ptch.Patch(color='b', label='RX')
+green_patch = ptch.Patch(color='g', label='RX Clean')
+plt.legend(handles=[red_patch, blue_patch, green_patch])
 
 
 plt.show()
